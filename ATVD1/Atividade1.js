109c28ef14a220b9d99ff9721decab9c17b78b2c
MCS-Lucas <lucascasilveira@gmail.com>
2024-08-23 10:49:44 -0300

    Atividade 1 feita

---
+++ ATVD1/Atividade1.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = 2;
+let num3;
+
+
+num3 = num1 + num2;
+console.log(num1 + " somado com " + num2 + " e " + num3);
+num3 = num1 - num2;
+console.log(num1 + " menos " + num2 + " e " + num3);
+num3 = num1 * num2;
+console.log(num1 + " vezes " + num2 + " e " + num3);
+num3 = num1 / num2;
+console.log(num1 + " dividido por " + num2 + " e " + num3);
